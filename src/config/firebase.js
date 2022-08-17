cb8add1a49bb9d83ae52503c117b09cdfd97a03f
Pratik Kale <ppvkale@gmail.com>
2022-08-18 01:17:29 +0530

    firebase migration

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,13 +5,13 @@ import 'firebase/firestore';
 import 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBKZCOx5O6x-7_lvJG3VFudJMjEy7DIgo8",
-  authDomain: "gdg-cloud-pune.firebaseapp.com",
-  projectId: "gdg-cloud-pune",
-  storageBucket: "gdg-cloud-pune.appspot.com",
-  messagingSenderId: "759500810714",
-  appId: "1:759500810714:web:e33a2ee4eae17e913c78e3",
-  measurementId: "G-D3B2KM9297"
+  apiKey: "AIzaSyBKh2dp5kpFyQrZ1H3kujfq4wRgtaJQHFs",
+  authDomain: "gdgpune-4728d.firebaseapp.com",
+  projectId: "gdgpune-4728d",
+  storageBucket: "gdgpune-4728d.appspot.com",
+  messagingSenderId: "736067899424",
+  appId: "1:736067899424:web:494a1725227d592aae2c45",
+  measurementId: "G-0XPWB173WL"
 };
 
 firebase.initializeApp(firebaseConfig);
